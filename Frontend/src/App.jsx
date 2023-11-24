707184f289fc5e6c2e60671a189fd64105fb8638
Saba <sabaazeem025@gmail.com>
2023-11-24 22:07:19 +0500

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import ShowBooks from './pages/ShowBooks.jsx';
-import CreateBook from './pages/CreateBook.jsx';
-import EditBook from './pages/EditBook.jsx';
-import DeleteBook from './pages/DeleteBook.jsx';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBooks />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
-    </Routes>
-  )
+
+  //Item list show code
+    const [items,setItems]=useState([]);
+    const [name,setName]=useState('');
+    useEffect(()=>{
+      axios.get('http://localhost:3000/todolist/')
+      .then((res)=>{
+        console.log(res.data.data);
+        setItems(res.data.data);
+      })
+      .catch((error)=>{
+        console.log(error.message);
+      })
+    },[name]);
+  //input show code
+    const handleSaveItem=()=>{
+        const data={
+            name,
+        };
+        
+            axios.post('http://localhost:3000/todolist/',data)
+        .then(()=>{
+            setName('');
+        })
+        .catch((error)=>{
+            console.log(error.message);
+        });
+    };
+  //handle delete item code
+    const handleDeleteItem=(id)=>{
+      axios.delete(`http://localhost:3000/todolist/${id}`)
+      .then(()=>{
+        setName('');
+      })
+      .catch((error)=>{
+        console.log(error.message);
+      })
+    }
+    return (
+      <div>
+        {items.map((item)=>(
+          <div>
+            <h1>{item.name}</h1>
+            <button onClick={()=>{
+              const id=item._id;
+              handleDeleteItem(id)
+            }}>Delete</button>
+          </div>
+        ))}
+        <div>
+          <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+          <button onClick={handleSaveItem}>Add</button>
+        </div>
+      </div>
+    )
 }
